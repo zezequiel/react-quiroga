04773cda073da17c3d40b9ee47a07bfb8379cf56
Ezequiel Quiroga <qezequiel96@gmail.com>
2022-07-26 18:52:56 -0300

    add products to firebase

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { db } from "../../firebase/firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { context } from "../Context/CartContext"; 
-const Cart = () =>{
-    const { products, removeItem, calculateTotal } = useContext(context);
 
+
+const Cart = () =>{
+    const { products, removeItem, calculateTotal, clear } = useContext(context);
+    const [idSale, setIdSale] = useState ("");
+
+    // Estados de formulario
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [direction, setDirection] = useState("");
+
+    const arrayPerson = [{nombre: name, apellido: lastName, email: email, direccion: direction}];
+
+    const checkout = () =>{
+            const saleCollection = collection(db, 'ventas');
+            addDoc(saleCollection, {
+                person: arrayPerson,
+                items: products,
+                date: serverTimestamp(),
+                total: calculateTotal()
+            })
+            .then((result) =>{
+                setIdSale(result.id)
+            })
+            alert("Su compra se concreto con exito")
+            clear();
+        
+    }
+
+    
 
     if(products.length === 0){
         return <h2>El carrito esta vacio, por favor hace click <Link to="/">acá</Link></h2>
@@ -22,7 +52,7 @@ const Cart = () =>{
                         <h2>Producto: {product.title}</h2>
                         <h2>Precio: ${product.price}</h2>
                         <h2>Cantidad: {product.qty}</h2>
-                        <button onClick={() =>removeItem(product.id)}>Eliminar producto</button>
+                        <button onClick={() =>removeItem(product.id)}>X</button>
                     </div>
                     
                 </div>
@@ -30,6 +60,62 @@ const Cart = () =>{
                 </div>
             ))}
             <h2>Precio total: ${calculateTotal()}</h2>
+            
+            <div className="formPurchase" onSubmit={checkout}>
+                <form>
+                    <label htmlFor="nombre">Nombre</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        name="nombre"
+                        value={name}
+                        placeholder="Ingresa tu nombre"
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                    />
+
+
+                    <label htmlFor="apellido">Apellido</label>
+                    <input
+                        name="apellido"
+                        value={lastName}
+                        type="text"
+                        placeholder="Ingresa tu apellido"
+                        required
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+
+
+                    <label htmlFor="email">Correo Electrónico</label>
+                    <input
+                        name="email"
+                        value={email}
+                        type="email"
+                        placeholder="ejemplo@gmail.com"
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+
+                    <label htmlFor="direccion">Direccion</label>
+                    <input
+                        name="direccion"
+                        value={direction}
+                        type="text"
+                        placeholder="Ingresa tu direccion"
+                        required
+                        onChange={(e) => setDirection(e.target.value)}
+                    />
+                        <input
+                            id="submit"
+                            type="submit"
+                            value="Finalizar Compra"
+                        />
+                    
+
+                </form>
+            </div>
+            
         </>
     )
 }
